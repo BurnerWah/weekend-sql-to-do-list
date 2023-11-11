5ae81fbe3465a06dee99a3b10b2fe121bd6adeb9
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-11-11 09:21:15 -0600

    Added rendering to client of todo list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -20,6 +20,41 @@ async function getTodos() {
  * Refreshes to do list items on the DOM
  * @param {TodoServerItem[]} todos
  */
-function renderTodos(todos) {}
+function renderTodos(todos) {
+  const ul = document.querySelector('ul#tasks')
+  ul.innerHTML = ''
+  for (const todo of todos) {
+    ul.appendChild(createTodoItem(todo))
+  }
+}
+
+/**
+ * Component to render a todo item
+ * @param {TodoServerItem} todo
+ * @returns {HTMLLIElement}
+ */
+function createTodoItem(todo) {
+  const li = document.createElement('li')
+  li.setAttribute('data-testid', 'toDoItem')
+  li.setAttribute('data-id', todo.id)
+
+  const text = document.createElement('span')
+  text.textContent = todo.text
+  li.appendChild(text)
+
+  const completeButton = document.createElement('button')
+  completeButton.setAttribute('data-testid', 'completeButton')
+  completeButton.textContent = 'Complete'
+  // TODO: Add event listener to complete todo item
+  li.appendChild(completeButton)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.setAttribute('data-testid', 'deleteButton')
+  deleteButton.textContent = 'Delete'
+  // TODO: Add event listener to delete todo item
+  li.appendChild(deleteButton)
+
+  return li
+}
 
 getTodos()
