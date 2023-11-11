31da454b97dabcc50b8cd52ab655c3e0dcbff306
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-11-11 08:52:53 -0600

    Added GET /todos route to router

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -3,4 +3,15 @@ const pool = require('../modules/pool')
 
 const router = Router()
 
+router.get('/', async (req, res) => {
+  // Got tired of the .tnen().catch() syntax for promises
+  try {
+    const result = await pool.query(`SELECT * FROM "todos";`)
+    res.send(result.rows)
+  } catch (err) {
+    console.error('Error executing query', err.stack)
+    res.sendStatus(500)
+  }
+})
+
 module.exports = router
